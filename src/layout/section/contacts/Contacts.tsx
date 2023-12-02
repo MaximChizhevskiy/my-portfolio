17a45ecccf703098afb34260579159983534a815
Maxim Chizhevskiy <max15.91@mail.ru>
2023-12-02 22:42:08 +0600

    Contacts refactor

--- src/layout/section/contacts/Contacts.tsx
+++ src/layout/section/contacts/Contacts.tsx
@@ -1,64 +1,21 @@
 import React from 'react';
-import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Button} from "../../../components/Button";
 import {Container} from "../../../components/Container";
-import {theme} from "../../../styles/Theme";
+import { S } from './Contacts_Styles';
 
-export const Contact = () => {
+export const Contact: React.FC = () => {
     return (
-        <StyledContacts>
+        <S.Contacts>
             <Container>
             <SectionTitle>Contact me</SectionTitle>
-            <StyledForm>
-                <Field placeholder={"name"}/>
-                <Field placeholder={"subject"}/>
-                <Field placeholder={"message"} as={"textarea"}/>
+            <S.Form>
+                <S.Field placeholder={"name"}/>
+                <S.Field placeholder={"subject"}/>
+                <S.Field placeholder={"message"} as={"textarea"}/>
                 <Button type={"submit"}>Send message</Button>
-            </StyledForm>
+            </S.Form>
             </Container>
-        </StyledContacts>
+        </S.Contacts>
     );
 };
-
-const StyledContacts = styled.section`
-  //height: 50vh;
-`
-
-const StyledForm = styled.form`
-  max-width: 500px;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  gap: 16px;
-  border-radius: 20px;
-  margin: 0 auto;
-  
-  textarea {
-    resize: none;
-    height: 150px;
-`
-
-const Field = styled.input`
-    width: 100%;
-    border: 1px solid ${theme.colors.accent};
-    background-color: ${theme.colors.primaryBg};
-  padding: 7px 15px;
-  border-radius: 6px;
-
-  color: ${theme.colors.titlesColor};
-  font-family: Poppins,sans-serif;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-  
-  &::placeholder {
-    text-transform: capitalize;
-  }
-  
-  &:focus-visible {
-    outline: 1px solid ${theme.colors.secondaryFontColor};
-  }
-`
---
+++ src/layout/section/contacts/Contacts_Styles.ts
@@ -0,0 +1,48 @@
+import {theme} from "../../../styles/Theme";
+import styled from "styled-components";
+
+const Contacts = styled.section``
+
+const Form = styled.form`
+  max-width: 500px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 16px;
+  border-radius: 20px;
+  margin: 0 auto;
+  
+  textarea {
+    resize: none;
+    height: 150px;
+`
+
+const Field = styled.input`
+    width: 100%;
+    border: 1px solid ${theme.colors.accent};
+    background-color: ${theme.colors.primaryBg};
+  padding: 7px 15px;
+  border-radius: 6px;
+
+  color: ${theme.colors.titlesColor};
+  font-family: Poppins,sans-serif;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  
+  &::placeholder {
+    text-transform: capitalize;
+  }
+  
+  &:focus-visible {
+    outline: 1px solid ${theme.colors.secondaryFontColor};
+  }
+`
+
+export const S = {
+    Contacts,
+    Form,
+    Field,
+}
